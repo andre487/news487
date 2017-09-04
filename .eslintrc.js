193e88856270e6600d350ee05c3c32e6eeeeb57d
andre487 <andre487@yandex-team.ru>
2017-09-04 17:40:37 +0300

    Infrastructure

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 8
+    },
+    env: {
+        node: true
+    },
+    rules: {
+        'no-console': 0
+    }
+};
