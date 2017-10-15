e128d596ac07dddf6aed48251543831e53649139
andre487 <andre487@yandex-team.ru>
2017-10-15 03:17:49 +0300

    Implement slow loading

--- web_ui/src/index.jsx
+++ web_ui/src/index.jsx
@@ -6,13 +6,7 @@ import {Provider} from 'react-redux';
 import App from '../containers/App';
 import configureStore from '../store/configureStore';
 
-// Needed for React Developer Tools
-window.React = React;
-
-// Needed for onTouchTap
-// Can go away when react 1.0 release
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
+window.stopPageLoadingRotation();
 injectTapEventPlugin();
 
 ReactDOM.render(
